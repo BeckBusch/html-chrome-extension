572d4d0a6e870a1934bba69ae34d15f90223a37c
Beck Busch <beckbusch5@gmail.com>
2016-09-02 08:25:52 +1200

    Create bookmarklet.js

---
+++ bookmarklet.js
@@ -0,0 +1,5 @@
+javascript:var element = document.createElement("link");
+element.setAttribute("rel", "stylesheet");
+element.setAttribute("type", "text/css");
+element.setAttribute("href", "http://beckbusch.github.io/html-chrome-extension/cursor.css");
+document.getElementsByTagName("head")[0].appendChild(element);
